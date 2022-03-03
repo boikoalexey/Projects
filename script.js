928da602b03edecff39603f09c82e8451e28f15d
Alexey Boyko <a.boyko@anviovr.com>
2022-03-03 11:43:39 +0300

    Lern JS

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ if (gender == 'M') {
 
 console.log(userGender) */
 
-// promt передает вкачестве значения строки
-let num = +prompt('Enter a number')
+// promt передает в качестве значения строки
+/* let num = +prompt('Enter a number')
 
 switch (num) {
     case 0:
@@ -69,4 +69,245 @@ switch (num) {
 
     default: 
         alert('Error')
+} */
+
+/* let a = 82
+let b = 32
+
+if (a > b) {
+    console.log(a)
+} else {
+    console.log(b)
+} */
+
+/* let monthNumber = +prompt('Enter month number')
+
+switch (monthNumber) {
+    case 1:
+        console.log('January')
+        break
+    case 2:
+        console.log('February')
+        break
+    case 3:
+        console.log('March')
+        break
+    case 4:
+        console.log('April')
+        break
+
+    default: 
+        alert('Error')
+}  */
+
+/* let circle = 25
+let square = 30
+
+if (circle > square) {
+    console.log('Yes')
+} else {
+    console.log('No')
+} */
+
+/* let sum = 0
+let number = 1
+
+while (number <= 100) {
+    sum = sum + number
+    number = number + 1
 }
+
+console.log(sum) */
+
+/* const input = +prompt('Enter a number')
+
+let fact = 1
+for (let number = 1; number <= input; number++) {
+    fact = fact * number
+}
+
+console.log(fact) */
+
+/* let sum = 0
+for (let number = 1; number <= 100; number++) {
+    sum = sum + number
+}
+
+console.log(sum) */
+
+/* let sum = 0
+
+while(true) {
+    const input = prompt('Enter a number or =')
+
+    if (input == '=') {
+        break
+    } else {
+        sum = sum + Number(input)
+    }
+}
+
+console.log(sum) */
+
+/* let sum = 0
+
+for (let num = 50; num <= 100; num++) {
+    sum = sum + num
+}
+
+console.log(sum) */
+
+/* let num = 7
+let sum
+
+for (let value = 1; value <= 9; value++) {
+
+    switch (value) {
+        case 1:
+            sum = num * value
+            console.log(sum)
+            break
+        case 2:
+            sum = num * value
+            console.log(sum)
+            break
+        case 3:
+            sum = num * value
+            console.log(sum)
+            break
+        case 4:
+            sum = num * value
+            console.log(sum)
+            break
+        case 5:
+            sum = num * value
+            console.log(sum)
+            break
+        case 6:
+            sum = num * value
+            console.log(sum)
+            break
+        case 7:
+            sum = num * value
+            console.log(sum)
+            break
+        case 8:
+            sum = num * value
+            console.log(sum)
+            break
+        case 9:
+            sum = num * value
+            console.log(sum)
+            break
+        }
+} */
+
+/* const auto = {
+    model: 'Mers',
+    year: 2012,
+    color: 'black'
+}
+
+const input = prompt('Enter a property') */
+
+/* switch(input) {
+    case 'model':
+        console.log(auto.model)
+        break
+    case 'year':
+        console.log(auto.year)
+        break
+    case 'color':
+        console.log(auto.color)
+        break
+    default: 
+        console.log('Not found')
+} */
+
+/* const value = auto[input]
+
+if (value) {
+    console.log(value)
+} else {
+    console.log('Not found')
+} */
+
+/* let user = {
+    age: 30,
+    gender: 'Mal'
+}
+
+let input = prompt('Enter')
+let hasEnter = input in user
+
+if (hasEnter) {
+    delete user[input]
+} else { 
+    user[input] = null
+}
+
+console.log(user) */
+
+/* const storGood = {
+    number: 1001,
+    place: 'A101',
+    weight: 25
+}
+
+const shopGood = Object.assign({}, storGood, {cost: 10})
+
+console.log(storGood, shopGood) */
+
+/* let person = {
+    name: 'Sam',
+    years: 25
+}
+
+let {name, years:age, height=null} = person
+
+console.log(name, age, height) */
+
+/* const user = {
+    name: 'John',
+}
+
+const input = prompt('Enter your age')
+user.age = input
+
+const admin = Object.assign({}, user, {role: 'admin'})
+let {name, age, role} = admin
+
+console.log(user, admin, name, age, role) */
+
+/* const nums = [10, 20, 30, 40, 50]
+
+const input = +prompt('Enter')
+const index = nums.indexOf(input)
+
+if (index > -1) {
+    console.log(index)
+} else {
+    console.log('Not found')
+} */
+
+/* const arr = []
+
+for (let i = 0; i < 5; i++) {
+    input = prompt('Enter')
+    arr.push(input)
+}
+
+console.log(arr) */
+
+/* const nums = [2,12,34,23,21]
+
+const input = +prompt('Enter')
+const index = nums.indexOf(input)
+
+if (index > -1) {
+    nums.splice(index, 1)
+} else {
+    console.log('Not found')
+}
+
+console.log(nums) */
